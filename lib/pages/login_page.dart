5f8bac9fb8d6471e58df3051391fb3bb05b263d9
Mariana Oliveira <87584257+marianaoliveira1@users.noreply.github.com>
2023-10-29 19:25:06 -0300

    .

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,54 @@
+import 'package:chat_messenger/components/my_text_filed.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Icon(
+                Icons.message,
+                size: 80,
+                color: Colors.grey[800],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "Welcome back you're been missed!",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              MyTextField(controller: emailController, hintText: "Email", obscureText: false),
+              SizedBox(
+                height: 13,
+              ),
+              MyTextField(controller: passwordController, hintText: "Password", obscureText: true),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
